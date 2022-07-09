e4c3ec076810b0f2097be5de05124e0e3ac5648b
Patrick Burns <patreeek@protonmail.com>
2022-07-09 12:58:07 -0400

    added shebang

--- create.py
+++ create.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 import common as cm
 from datetime import datetime as dt
 
--- verify.py
+++ verify.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 import common as cm
 
 parser = cm.ChecksumsArguments(prog='checksums-verify')
